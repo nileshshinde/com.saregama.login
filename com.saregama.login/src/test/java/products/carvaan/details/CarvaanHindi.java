2779f7055f91caf3407558c7c9671e8d80b77b37
niles <niles@host.docker.internal>
2023-03-18 14:42:10 +0530

    commit

--- com.saregama.login/src/test/java/products/carvaan/details/CarvaanHindi.java
+++ com.saregama.login/src/test/java/products/carvaan/details/CarvaanHindi.java
@@ -1,7 +1,39 @@
 package products.carvaan.details;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+
 public class CarvaanHindi {
 	
-	private void test1() {}
+	private WebDriver driver;
 
+	@BeforeTest
+	public void beforeTest() {
+		ChromeOptions chromeoptions = new ChromeOptions();
+		chromeoptions.addArguments("--disable-extensions");
+		chromeoptions.addArguments("--headless");
+		chromeoptions.addArguments("--disable-gpu");
+		chromeoptions.addArguments("--no-sandbox");
+		
+		//WebDriverManager.chromedriver().setup();
+		WebDriverManager.chromedriver().browserVersion("111.0.5563.65").setup();
+		
+		driver = new ChromeDriver(chromeoptions);
+		
+		driver.get("https://www.saregama.com/carvaan/hindi/buy-now");
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
 }
